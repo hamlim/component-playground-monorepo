8df53a03557175bface6852e0140e9dde85b6e67
Matt Hamlin <hamlim@outlook.com>
2019-06-04 20:33:40 -0400

    Add readme, cleanup index.js file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Component Playground
+
+Concept: Developing a UI through code and visual editing experiences all on the web.
+
+### TODO:
+
+- [ ] Structure a POC for the node editor
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p>Welcome to the component playground repo</p>
     </div>
   );
 }
